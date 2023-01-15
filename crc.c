b8bd9c73d0317b2a01cddc5aabc88bd68799a75b
SivaPullaiah <mangalisivapullaiah@gmail.com>
2023-01-15 21:01:57 +0530

    Cyclic Redundency Check

---
+++ crc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+	int i,j,lg,lm;
+	char m[100],q[100],r[30],g[30],g1[30];
+	printf("Enter the binary form of M(x): ");
+	gets(m);
+	printf("Enter data in binary form of g(x): ");
+	get(g)
+	lg=strlen(g);
+	lm=strlen(m);
+	strcpy(lg,g);
+	
+}
